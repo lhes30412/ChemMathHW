d133496f6996aa236681dab4c9386a6ad79006bb
lhes30412 <lhes30412@gmail.com>
2019-10-03 01:20:34 +0800

    Add np.where part

--- HW1.py
+++ HW1.py
@@ -1,10 +1,12 @@
 import numpy as np
+import time
 from scipy.spatial import distance_matrix
 
 # For 2D circle
 N = 100000000
 count = 0
 # Generate random points and origin point
+t = time.time()
 random_point = np.random.random([N, 2])
 origin = np.array([[0, 0]])
 
@@ -17,9 +19,10 @@ for i in range(N):
 prob = count/N
 pi = prob*4
 print(pi)
-print('Program works successfully, go buy some beers!!')
+print('Total time taken for calculating pi by for loops in 2D model: %.3f sec' % (time.time() - t))
 
-# for 3D
+# for 3D model
+t = time.time()
 count = 0
 random_point = np.random.random([N, 3])
 origin = np.array([[0, 0, 0]])
@@ -32,4 +35,40 @@ for i in range(N):
 prob = count/N
 pi = prob*6
 print(pi)
-print('Program works successfully again!! More beers!!')
+print('Total time taken for calculating pi by for loops in 3D model: %.3f sec' % (time.time() - t))
+
+# Test by using np.where
+# For 2D circle
+count = 0
+# Generate random points and origin point
+t = time.time()
+random_point = np.random.random([N, 2])
+origin = np.array([[0, 0]])
+
+dist = distance_matrix(origin, random_point)
+
+within_points = np.where(dist[0] < 1)
+shape = np.shape(within_points)
+count = shape[1]
+
+prob = count/N
+pi = prob * 4
+print(pi)
+print('Total time taken for calculating pi by numpy.where in 2D model: %.3f sec' % (time.time() - t))
+# for 3D
+t = time.time()
+count = 0
+random_point = np.random.random([N, 3])
+origin = np.array([[0, 0, 0]])
+
+dist = distance_matrix(origin, random_point)
+
+within_points = np.where(dist[0] < 1)
+shape = np.shape(within_points)
+count = shape[1]
+
+prob = count/N
+pi = prob*6
+print(pi)
+print('Total time taken for calculating pi by numpy.where in 3D model: %.3f sec' % (time.time() - t))
+print('Program works successfully!! Go buy some beer!!')
